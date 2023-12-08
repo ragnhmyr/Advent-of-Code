e20a35ab296a0906e180f758cd18f1ad6a376613
ragnhmyr <ragnhild_myrnes@hotmail.com>
2023-12-08 05:09:44 +0100

    day 5 more effective, part 2 still not running through

--- 2023/5_seeds.py
+++ 2023/5_seeds.py
@@ -2,49 +2,19 @@ file_name="5_input_test.txt"
 
 def get_location(seed_number, total_mapping_list):
     start_number = seed_number
-    print("SEED NUMBER:", start_number)
+    #print("SEED NUMBER:", start_number)
     for i in range(len(total_mapping_list)):
         this_mapping = total_mapping_list[i]
-        print("going to next mapping")
-        mapping_dict = {}
+        #print("going to next mapping")
+        #mapping_dict = {}
         for j in range(len(this_mapping)):
             dest_range = (this_mapping[j][0], this_mapping[j][0]+this_mapping[j][2])
             source_range = (this_mapping[j][1], this_mapping[j][1]+this_mapping[j][2])
-            mapping_dict[source_range] = dest_range
-
-        for source_range, dest_range in mapping_dict.items():
             if source_range[0] <= start_number < source_range[1]:
                 start_number = dest_range[0] + (start_number - source_range[0])
-                print("NEW START NUMBER", start_number)
+                #print("NEW START NUMBER", start_number)
                 break
-        # total_dest = []
-        # total_source = []
-        # print("THIS MAPPING", this_mapping)
-        # print("This mampping length", len(this_mapping))
-        #print("going to next mapping")
-        # for j in range(len(this_mapping)):
-        #     dest_list = list(range(this_mapping[j][0],this_mapping[j][0]+this_mapping[j][2]))
-        #     source_list = list(range(this_mapping[j][1],this_mapping[j][1]+this_mapping[j][2]))
-        #     #print("DEST LIST", dest_list)
-        #     #print("SOURCE LIST", source_list)
-
-        #     # for k in range(len(dest_list)): #source and dest are the same length
-        #     #     total_dest.append(dest_list[k])
-        #     #     total_source.append(source_list[k])
-        #     for r in range(len(dest_list)):
-        #         #print(total_source[r], total_dest[r])
-        #         if start_number == source_list[r]:
-        #             start_number = dest_list[r]
-        #             #print("NEW START NUMBER", start_number)
-        #             i+=1
-        #             j+=1
-        #             break #break and go to next mapping
-        #     else:
-        #         continue
-        #    break
-        # for p in range(len(total_source)):
-        #     print(total_source[p], total_dest[p])
-    print("Location: ",start_number)
+    #print("Location: ",start_number)
     return start_number
 
 with open(file_name) as f:
@@ -53,10 +23,6 @@ with open(file_name) as f:
     #first line are the seeds
     seeds_txt = lines[0].split(":")[1].split()
     seeds_number = [int(i) for i in seeds_txt]
-
-    all_seeds = []
-    for i in range(0,len(seeds_number),2):
-        all_seeds.extend(list(range(seeds_number[i], seeds_number[i] + seeds_number[i+1])))
 
     #create list of list for all the different mappings
     this_map = []
@@ -76,13 +42,21 @@ with open(file_name) as f:
             this_map=[]
 
     #print(total_mapping_list)
+    all_ranges = []
+    for i in range(0, len(seeds_number), 2):
+        all_ranges.append(range(seeds_number[i], seeds_number[i] + seeds_number[i+1]))
+    print(all_ranges)
 
     #have to divide into lowest location
-    locations = []
-    for i in range(len(all_seeds)):
-        location = get_location(all_seeds[i], total_mapping_list)
-        locations.append(location)
+    min_location = 0
+    for seed_range in all_ranges:
+        for seed in seed_range:
+            location = get_location(seed, total_mapping_list)
+            if min_location == 0: #just to set the firsto one
+                min_location = location
+            elif location < min_location:
+                min_location = location #set new location
 
-    print(locations)
-    print("Lowest location", min(locations))
+    #print(locations)
+    print("Min location:", min_location)
 
