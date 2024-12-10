bd8bb1f14f655ee7e593d6dfb9c8a5746b072c48
Ragnhild Myrnes <ragnhild_myrnes@hotmail.com>
2024-12-10 03:05:06 +0000

    day 9 part 1 complete

--- 2024/9_diskmap.py
+++ 2024/9_diskmap.py
@@ -1,12 +1,12 @@
 import time
 
-diskmap = "2333133121414131402" # this is the test input
+#diskmap = "2333133121414131402" # this is the test input
 start_time = time.time()
-# diskmap = ""
-# with open('9_input.txt') as f:
-#     for line in f:
-#         diskmap = line.strip() #only one line
-# print(diskmap)
+diskmap = ""
+with open('9_input.txt') as f:
+    for line in f:
+        diskmap = line.strip() #only one line
+print(diskmap)
 disk_dict = {}
 id = 0
 total_spaces = 0
@@ -14,17 +14,19 @@ for i in range(0, len(diskmap), 2):
     #might be an odd number where the last digit has no space
     size = diskmap[i]
     space = diskmap[i+1] if i+1 < len(diskmap) else 0  # Default space to 0 if last item has no space
-    
     #print("id:", id, "diskmap[i]:", size, "diskmap[i+1]:", space)
     disk_dict[id] = {"size": int(size), "space": int(space)}
     total_spaces += int(space)
     id += 1
 
-start_string = ""
+start_string = []
 only_numbers = [str(key) for key in disk_dict for _ in range(int(disk_dict[key]["size"]))]
 only_numbers.reverse() #reverse the numbers to find which one to insert first
 for key in disk_dict:
-    start_string += disk_dict[key]["size"]*str(key) + disk_dict[key]["space"]*"."
+    # Add numbers based on size
+    start_string.extend([str(key)] * int(disk_dict[key]["size"]))
+    # Add empty spaces (.) based on space
+    start_string.extend(["."] * int(disk_dict[key]["space"]))
         
 string_list = list(start_string)
 length_stringlist = len(string_list)
@@ -44,6 +46,7 @@ checksum = 0
 for i in range(len(string_list)):
     checksum += i*int(string_list[i])
 #90427061447 was too low - forgot to take into account that numbers have more than one digit above 9
-#17548852145461 was too high
+#17548852145461 was too high - forgot to take into account that numbers have more than one digit in the string list
+#6349606724455 correct - 15 sec
 print("Checksum:", checksum)
 print("Process finished --- %s seconds ---" % (time.time() - start_time)) # part 1 took 65 seconds
