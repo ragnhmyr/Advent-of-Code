0a4c67103bc3b841bdd09070345c98741308772b
Ragnhild Myrnes <ragnhild_myrnes@hotmail.com>
2024-12-11 01:13:44 +0000

    day 10 finally understood part 1

--- 2024/10_hiking_trail.py
+++ 2024/10_hiking_trail.py
@@ -1,5 +1,5 @@
 map = []
-with open('10_input_test_medium.txt') as f:
+with open('10_input.txt') as f:
     for line in f:
         map.append([int(x) for x in line.strip()]) #only one line
 no_rows = len(map)
@@ -32,66 +32,57 @@ def check_if_neighbour_is_on_trail(this_location,neighbour_location,map):
 
 def count_hiking_trails(map, start_loc, no_rows, no_columns):
     # If reached the end of the trail return 1 as we found a valid path
-    print("Looking at value",map[start_loc[0]][start_loc[1]],"at location",start_loc)
+    #print("Looking at value",map[start_loc[0]][start_loc[1]],"at location",start_loc)
     if map[start_loc[0]][start_loc[1]] == 9:
-        print("Found the end of a path, returning 9")
+        #print("Found the end of a path, returning 9")
         return 1
 
     total_paths = 0
     neighbours = find_valid_neighbours(start_loc, no_rows, no_columns)
     for neighbour in neighbours:
-        print("Found a neighbour at ",neighbour, "neighbour value is",map[neighbour[0]][neighbour[1]])
+        #print("Found a neighbour at ",neighbour, "neighbour value is",map[neighbour[0]][neighbour[1]])
         if check_if_neighbour_is_on_trail(start_loc, neighbour, map):
-            print("Neighbour is on the trail")
+            #print("Neighbour is on the trail")
             # Continue searching from the valid neighbour
             total_paths += count_hiking_trails(map, neighbour, no_rows, no_columns)
 
     return total_paths
 
 def count_hiking_trails_part1(map, start_loc, no_rows, no_columns):
-    # If reached the end of the trail return 1 as we found a valid path
-    print("Looking at value",map[start_loc[0]][start_loc[1]],"at location",start_loc)
-    if map[start_loc[0]][start_loc[1]] == 9:
-        print("Found the end of a path, returning 9")
-        return 1
+    to_visit = [start_loc]
+    reached_end_points = set()
 
-    total_paths = 0
-    neighbours = find_valid_neighbours(start_loc, no_rows, no_columns)
-    for neighbour in neighbours:
-        print("Found a neighbour at ",neighbour, "neighbour value is",map[neighbour[0]][neighbour[1]])
-        if check_if_neighbour_is_on_trail(start_loc, neighbour, map):
-            print("Neighbour is on the trail")
-            # Continue searching from the valid neighbour
-            total_paths += count_hiking_trails(map, neighbour, no_rows, no_columns)
+    while to_visit:
+        current_loc = to_visit.pop()
+        current_value = map[current_loc[0]][current_loc[1]]
 
-    return total_paths
+        if current_value == 9:
+            reached_end_points.add(current_loc)
+            continue
 
-def main_part_1(map,no_rows,no_columns):
+        for neighbour in find_valid_neighbours(current_loc, no_rows, no_columns):
+            if map[neighbour[0]][neighbour[1]] == current_value + 1:
+                to_visit.append(neighbour)
+
+    return len(reached_end_points)
+
+def main(map,no_rows,no_columns):
     start_locations = []
     for i in range(no_rows):
         for j in range(no_columns):
             if map[i][j] == 0:
                 start_locations.append((i,j))
-    ranks_per_start = []
+    ranks_per_start_1 = []
+    ranks_per_start_2 = []
     for start_loc in start_locations:
-        print("*******")
-        print("Checking start location",start_loc)
-        ranks_per_start.append(count_hiking_trails_part1(map,start_loc,no_rows,no_columns))
-        print(ranks_per_start)
+        #print("*******")
+        #print("Checking start location",start_loc)
+        ranks_per_start_1.append(count_hiking_trails_part1(map,start_loc,no_rows,no_columns))
+        ranks_per_start_2.append(count_hiking_trails(map,start_loc,no_rows,no_columns))
+    print("part 1 ranks per start: ", ranks_per_start_1)
+    print("part 1 ranks sum: ", sum(ranks_per_start_1))
+    print("part 2 ranks per start", ranks_per_start_2)
+    print("part 2 sum of ranks",sum(ranks_per_start_2))
 
-def main_part_2(map,no_rows,no_columns):
-    start_locations = []
-    for i in range(no_rows):
-        for j in range(no_columns):
-            if map[i][j] == 0:
-                start_locations.append((i,j))
-    ranks_per_start = []
-    for start_loc in start_locations:
-        print("*******")
-        print("Checking start location",start_loc)
-        ranks_per_start.append(count_hiking_trails(map,start_loc,no_rows,no_columns))
-        print(ranks_per_start)
 
-main_part_1(map,no_rows,no_columns)
-
-main_part_2(map,no_rows,no_columns)
+main(map,no_rows,no_columns)
