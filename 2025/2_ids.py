1ac5d4a62646f9f32d63f0105fb5e72129d0dbce
ragnhmyr <ragnhild_myrnes@hotmail.com>
2025-12-02 14:22:15 +0000

    day 2 part 2

--- 2025/2_ids.py
+++ 2025/2_ids.py
@@ -1,21 +1,45 @@
-instructions = []
-with open('2_input_test.txt') as f:
-    for line in f:
-        split_id = line.strip().split(',')
-        instructions = split_id
+def open_read_file(file_name):
+    instructions = []
+    with open(file_name) as f:
+        for line in f:
+            split_id = line.strip().split(',')
+            instructions = split_id
+    return instructions
 
-invalid_ids = []
-for instruction in instructions:
-    start,stop = instruction.split('-')
-    start = int(start)
-    stop = int(stop)
-    for num in range(start, stop + 1):
-        str_num = str(num)
-        first_half = str_num[:len(str_num)//2]
-        second_half = str_num[len(str_num)//2:]
-        #print(f'Checking ID: {num}, First half: {first_half}, Second half: {second_half}')
-        if first_half == second_half:
-            invalid_ids.append(num)
-            #print(f'\nInvalid ID found: {num}')
+##part 1
+def sum_invalid_ids_part_one(instructions):
+    invalid_ids = []
+    for instruction in instructions:
+        start,stop = instruction.split('-')
+        start = int(start)
+        stop = int(stop)
+        for num in range(start, stop + 1):
+            str_num = str(num)
+            first_half = str_num[:len(str_num)//2]
+            second_half = str_num[len(str_num)//2:]
+            if first_half == second_half:
+                invalid_ids.append(num)
+    return sum(invalid_ids)
 
-print("Sum of invalid IDs:", sum(invalid_ids))
+##part 2
+def sum_invalid_ids_part_two(instructions):
+    invalid_ids = []
+    for instruction in instructions:
+        start,stop = instruction.split('-')
+        start = int(start)
+        stop = int(stop)
+        for num in range(start, stop + 1):
+            str_num = str(num)
+            for i in range((len(str_num)//2)+1):
+                length = len(str_num)//2
+                sub_string = str_num[0:i]
+                if sub_string != '':
+                    count_substring = str_num.count(sub_string)
+                    if count_substring >= 2 and (len(sub_string) == len(str_num)/count_substring):
+                        invalid_ids.append(num)
+                        break
+            
+    return sum(invalid_ids)
+
+instructions = open_read_file('2_input.txt')
+print("Sum of invalid IDs:", sum_invalid_ids_part_two(instructions))
