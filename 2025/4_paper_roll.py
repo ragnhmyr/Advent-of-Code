69356e422bce0da1f5a19ed29871188c1bd449f5
ragnhmyr <ragnhild_myrnes@hotmail.com>
2025-12-06 01:51:17 +0000

    part 2 day 4

--- 2025/4_paper_roll.py
+++ 2025/4_paper_roll.py
@@ -42,16 +42,15 @@ def part1():
     print(f"Total valid forklift positions: {total_valid_positions}")
 
 def part2():
-    file_name = '4_input_test.txt'
+    file_name = '4_input.txt'
     lines = read_roll_file(file_name)
     no_columns = len(lines[0])
-    print("Number of rows:", len(lines))
-    print("Number of columns:", no_columns)
     total_valid_positions = 0
     new_lines = [row[:] for row in lines]  # copy each inner list
     iteration = 0
     this_iteration_removed = 0
     while this_iteration_removed != 0 or iteration == 0:
+        this_iteration_removed = 0
         for i in range(len(lines)):
             for j in range(no_columns):
                 count_adjacent_rolls = 0
@@ -61,8 +60,6 @@ def part2():
                 for x, y in adjacent_positions: #find valid positions
                     if 0 <= x < len(lines) and 0 <= y < no_columns:
                         if lines[x][y] == '@':
-                            #print("Adjacent roll found at:", (x, y))
-                            #print("Adjacent roll position value:", lines[x][y])
                             count_adjacent_rolls += 1
                             if count_adjacent_rolls >= 4:
                                 #j+=1
@@ -70,17 +67,14 @@ def part2():
                 #checking if valid position - then it is a valid position for forklift
                 if count_adjacent_rolls < 4 and lines[i][j] == '@':
                     new_lines[i][j] = 'X'
-                    #print("Found valid forklift position at:", (i, j))
-                    #print_rolls(new_lines)
                     this_iteration_removed += 1
         lines = [row[:] for row in new_lines] 
         iteration += 1
         total_valid_positions += this_iteration_removed
-        this_iteration_removed = 0
         
     print_rolls(new_lines)
     print(f"Total valid forklift positions: {total_valid_positions}")
 
 if __name__ == "__main__":
-    #part1()
+    part1()
     part2()
