21792fedaa0ce5a9ac384e9c91a6932d66ba8988
ragnhmyr <ragnhild_myrnes@hotmail.com>
2025-12-06 02:40:57 +0000

    part1 day 5

---
+++ 2025/5_fresh_ingredients.py
@@ -0,0 +1,37 @@
+def read_ingredient_file(filename: str):
+    ranges = []
+    lines = []
+    ingredients = []
+    with open(filename, 'r') as file:
+        lines = [line.strip() for line in file.readlines()]
+    for line in lines:
+        if "-" in line:
+            parts = line.split("-")
+            start = int(parts[0])
+            end = int(parts[1]) + 1 # to include the end value
+            ranges.append(range(start,end))
+        elif len(line) > 0:
+            ingredients.append(int(line))
+    
+    return ranges, ingredients
+
+#takes too long on the proper input
+def combine_ranges(ranges):
+    combined = set()
+    for r in ranges:
+        combined.update(r)
+    return combined
+
+def part1():
+    file_name = "5_input.txt"
+    ranges, ingredients = read_ingredient_file(file_name)
+    valid_ingredients = 0
+    for ingredient in ingredients:
+        for r in ranges:
+            if ingredient in r:
+                valid_ingredients += 1
+                break
+    print("Sum of valid ingredients:", valid_ingredients)
+
+if __name__ == "__main__":
+    part1()
