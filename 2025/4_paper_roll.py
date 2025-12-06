38eb99dfd47dff8cb44ab5a68a7c007a7c06e2b3
ragnhmyr <ragnhild_myrnes@hotmail.com>
2025-12-06 01:26:29 +0000

    part 1 day 4

--- 2025/4_paper_roll.py
+++ 2025/4_paper_roll.py
@@ -3,13 +3,19 @@ def read_roll_file(filename: str) -> list[str]:
         lines = [list(line.strip()) for line in file.readlines()]
     return lines
 
+def print_rolls(rolls: list[list[str]]) -> None:
+    for row in rolls:
+        print(''.join(row))
+    print("\n")
+
 def main():
-    file_name = '4_input_test.txt'
+    file_name = '4_input.txt'
     lines = read_roll_file(file_name)
     no_columns = len(lines[0])
+    print("Number of rows:", len(lines))
     print("Number of columns:", no_columns)
     total_valid_positions = 0
-    new_lines = lines.copy()
+    new_lines = [row[:] for row in lines]  # copy each inner list
     for i in range(len(lines)):
         for j in range(no_columns):
             count_adjacent_rolls = 0
@@ -19,20 +25,20 @@ def main():
             for x, y in adjacent_positions: #find valid positions
                 if 0 <= x < len(lines) and 0 <= y < no_columns:
                     if lines[x][y] == '@':
-                        print("Adjacent roll found at:", (x, y))
-                        print("Adjacent roll position value:", lines[x][y])
+                        #print("Adjacent roll found at:", (x, y))
+                        #print("Adjacent roll position value:", lines[x][y])
                         count_adjacent_rolls += 1
                         if count_adjacent_rolls >= 4:
                             #j+=1
                             break
             #checking if valid position - then it is a valid position for forklift
-            if count_adjacent_rolls < 4:
+            if count_adjacent_rolls < 4 and lines[i][j] == '@':
                 new_lines[i][j] = 'X'
-                print("Found valid forklift position at:", (i, j))
-                print(new_lines)
-            total_valid_positions += 1
+                #print("Found valid forklift position at:", (i, j))
+                #print_rolls(new_lines)
+                total_valid_positions += 1
             
-    print(new_lines)
+    print_rolls(new_lines)
     print(f"Total valid forklift positions: {total_valid_positions}")
 
 if __name__ == "__main__":
