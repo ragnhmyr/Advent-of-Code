e2f1c175af7d1bdfb73b85311e8a3acd329bdebd
ragnhmyr <ragnhild_myrnes@hotmail.com>
2025-12-08 02:26:37 +0000

    day 7 start

---
+++ 2025/7_beam_splitter.py
@@ -0,0 +1,39 @@
+def read_file(filename: str):
+    lines = []
+    with open(filename, 'r') as file:
+        lines = [(line.strip()) for line in file.readlines()]
+    return lines
+
+def find_start_position(line):
+    for i in range(len(line)):
+        if line[i] == "S":
+            return i
+        
+def print_char_list(list):
+    for elem in list:
+        text = "".join(elem)
+        print(text)
+    print("\n")
+    
+def count_total_splits(char_list):
+    #if `|` is above and to both sides of '^' count the split
+    return
+
+def part1():
+    filename = "7_input_test.txt"
+    lines = read_file(filename)
+    start_index = find_start_position(lines[0])
+    char_list = [list(line) for line in lines[1:]]
+    char_list[0][start_index]="|"
+    print("Len char list", len(char_list))
+    for i in range(1,len(char_list)):
+        for j in range(len(char_list[i])):
+            if char_list[i][j] == "^" and char_list[i-1][j] == "|" and j in range(1,len(char_list[i])-1): #assumes that ^ cant be next to another ^ 
+                char_list[i][j-1] = "|"
+                char_list[i][j+1] = "|"
+            elif char_list[i][j] == "." and char_list[i-1][j] == "|":
+                char_list[i][j] = "|"
+            print_char_list(char_list)
+
+if __name__ == "__main__":
+    part1()
