1fd4c6a53b0335ecd9f12be83df9df93d541da66
ragnhmyr <ragnhild_myrnes@hotmail.com>
2025-12-08 04:33:32 +0000

    start day 7 part 2

--- 2025/7_beam_splitter.py
+++ 2025/7_beam_splitter.py
@@ -1,3 +1,5 @@
+import copy 
+
 def read_file(filename: str):
     lines = []
     with open(filename, 'r') as file:
@@ -11,7 +13,7 @@ def find_start_position(line):
         
 def print_char_list(list):
     for elem in list:
-        text = "".join(elem)
+        text = "".join(str(x) for x in elem)
         print(text)
     print("\n")
     
@@ -25,7 +27,7 @@ def count_total_splits(char_list):
     return total_splits
 
 def part1():
-    filename = "7_input.txt"
+    filename = "7_input_test.txt"
     lines = read_file(filename)
     start_index = find_start_position(lines[0])
     char_list = [list(line) for line in lines[1:]]
@@ -40,6 +42,22 @@ def part1():
                 char_list[i][j] = "|"
     print_char_list(char_list)
     print("Total splits ", count_total_splits(char_list))
+    return char_list
+
+def part2(char_list):
+    new_char_list = [[0 if x == "|" else x for x in sublist] for sublist in char_list]
+    print(new_char_list)
+    print_char_list(new_char_list)
+    for i in range(1,len(char_list)):
+        for j in range(len(char_list[i])):
+            if char_list[i][j]=="|" and (char_list[i][j+1] or char_list[i][j+1] == "^"):
+                new_char_list[i][j] +=1
+                print_char_list(new_char_list)
+            if char_list[i-1][j] == "|":
+                new_char_list[i][j] +=1
+                print_char_list(new_char_list)
+    print_char_list(new_char_list)
 
 if __name__ == "__main__":
-    part1()
+    part2_start = part1()
+    part2(part2_start)
